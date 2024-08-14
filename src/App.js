f8c787c1faa871ef39c298c02eb2344134ae9cb2
sara-34 <saritaariaslondono12@gmail.com>
2024-08-14 12:06:13 -0500

    Subiendo cambios

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import './App.css';
 function Session({ title, description, whatsappLink }) {
   return (
     <div className="Session">
-      <h2>{title}</h2>
-      <p>{description}</p>
+     
       <a href={whatsappLink} target="_blank" rel="noopener noreferrer" className="whatsapp-button">
         Chat en WhatsApp
       </a>
@@ -20,27 +19,52 @@ function App() {
       description: 'Descripción de la sesión 1.',
       whatsappLink: 'https://wa.me/3122587675'
     },
-    {
-      title: 'Sesión 2',
-      description: 'Descripción de la sesión 2.',
-      whatsappLink: 'https://wa.me/3122587675'
-    },
+    
     // Puedes añadir más sesiones aquí
   ];
 
   return (
     <div className="App">
+      <nav className='nav-class'>
+        <div className='lista'>
+          <div>
+            <ul className='lista-nav'>
+              <li><a href='#'>Inicio</a></li>
+              <li><a href='#'>Cabañas</a></li>
+              <li><a href='#'>Contacto</a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+
       <header className="App-header">
-        <h1>Mis Sesiones</h1>
+  <h1>CABAÑAS JARDINES DEL TATAMA</h1>
+  <div className="banner">
+  <img
+  src="./imagenes/Imagen de WhatsApp 2024-08-12 a las 22.39.35_d5a35b4e.jpg"
+  
+/>
+
+  </div>
+</header>
+
+
+
+
+
+
+
+
+
+      <footer className="App-header">
+        <h1>INFORMACION</h1>
         {sessions.map((session, index) => (
           <Session
-            key={index}
-            title={session.title}
-            description={session.description}
             whatsappLink={session.whatsappLink}
           />
         ))}
-      </header>
+      </footer>
     </div>
   );
 }
