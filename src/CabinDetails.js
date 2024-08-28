61a53475454eff54ad34fabf55732aab640a6fbe
sara-34 <saritaariaslondono12@gmail.com>
2024-08-28 10:53:40 -0500

    Subiendo archivos

--- src/CabinDetails.js
+++ src/CabinDetails.js
@@ -1,34 +1,57 @@
-import React from 'react';
-import './CabinDetails.css'; // Asegúrate de que este archivo CSS exista
+import React, { useState } from 'react';
+import './CabinDetails.css';
 
 function CabinDetails({ cabin, onClose }) {
+  const [selectedImage, setSelectedImage] = useState(null);
   const whatsappMessage = `Hola, estoy interesado en reservar la ${cabin.name}.`;
 
+  const handleImageClick = (image) => {
+    setSelectedImage(image);
+  };
+
+  const closeImageModal = () => {
+    setSelectedImage(null);
+  };
+
   return (
-    <div className="cabin-details">
-      
-      <h3>{cabin.name}</h3>
-      <div className="cabin-details-images">
-        {cabin.images.map((image, index) => (
-          <img key={index} src={image} alt={`Imagen de ${cabin.name}`} className="cabin-details-img" />
-        ))}
+    <>
+      <div className="cabin-details-overlay" onClick={onClose}></div>
+      <div className="cabin-details">
+        <h3>{cabin.name}</h3>
+        <div className="cabin-details-images">
+          {cabin.images.map((image, index) => (
+            <img
+              key={index}
+              src={image}
+              alt={`Imagen de ${cabin.name}`}
+              className="cabin-details-img"
+              onClick={() => handleImageClick(image)}
+            />
+          ))}
+        </div>
+        <p>{cabin.description}</p>
+        <h2>ADICIONALES</h2>
+        <p>{cabin.adicional}</p>
+
+        <a
+          href={`https://wa.me/3122587675?text=${encodeURIComponent(whatsappMessage)}`}
+          className="reserve-button"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Reservar
+        </a>
+        <button onClick={onClose} className="close-button">Cerrar</button>
+
+        {selectedImage && (
+          <div className="image-modal" onClick={closeImageModal}>
+            <img src={selectedImage} alt="Imagen ampliada" className="image-modal-content" />
+          </div>
+        )}
       </div>
-      <p>{cabin.description}</p>
-      <h2>ADICIONALES</h2>
-      <p>{cabin.adicional}</p>
-     
-      <a 
-        href={`https://wa.me/3122587675?text=${encodeURIComponent(whatsappMessage)}`} // Enlace con mensaje predefinido
-        className="reserve-button" 
-        target="_blank" 
-        rel="noopener noreferrer"
-      >
-        
-        Reservar
-      </a>
-      <button onClick={onClose} className="close-button">Cerrar</button>
-    </div>
+    </>
   );
 }
 
 export default CabinDetails;
+
